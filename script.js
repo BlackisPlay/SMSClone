c41bfe11e99df9c0ec010adc82b601f61b6e9beb
BlackisPlay <102598445+BlackisPlay@users.noreply.github.com>
2026-03-12 18:02:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,40 @@ let backgroundImage = document.getElementById("intro");
 let fullscreenButton = document.getElementById("goFullscreen");
 let statsHider = document.getElementById("statsHider");
 let goBack = document.getElementById("goBack");
+let goBack2 = document.getElementById("goBack2");
+let validTicket = document.getElementById("validTicket");
+
+function updateTicketTime() {
+
+    const now = new Date();
+
+    // start time = 5 minutes ago
+    const start = new Date(now.getTime() - 5 * 60 * 1000);
+
+    // end time = 1 hour after start
+    const end = new Date(start.getTime() + 60 * 60 * 1000);
+
+    const day = String(start.getDate()).padStart(2,'0');
+    const month = String(start.getMonth() + 1).padStart(2,'0');
+    const year = start.getFullYear();
+
+    const startH = String(start.getHours()).padStart(2,'0');
+    const startM = String(start.getMinutes()).padStart(2,'0');
+
+    const endH = String(end.getHours()).padStart(2,'0');
+    const endM = String(end.getMinutes()).padStart(2,'0');
+
+    document.getElementById("validTicket").innerHTML = `
+        DPMK,&nbsp;a.s.<br>
+        SMS&nbsp;prestupny&nbsp;CL&nbsp;1.50&nbsp;EUR.<br>
+        Platnost&nbsp;od&nbsp;<span class="time">${day}.${month}.${year}&nbsp; 
+        ${startH}:${startM}</span>&nbsp;do<br>
+        <span class="time">${endH}:${endM}</span>&nbsp;hod. <br>
+        dQhZwowF
+    `;
+}
+
+updateTicketTime();
 
 function startClickedAnimation(){
     clickIndicator.style.opacity = 0.2;
@@ -17,7 +51,9 @@ chatSelector.addEventListener("click", () => {
         clickIndicator.style.opacity = 0;
         fullscreenButton.style.display = "none";
         goBack.style.display = "block";
+        goBack2.style.display = "block";
         intro.src = "chat.jpg";
+        validTicket.style.opacity = 1;
     }, 200);
 });
 
@@ -26,9 +62,22 @@ goBack.addEventListener("click", () => {
         statsHider.style.backgroundColor = "#1f1e23";
         fullscreenButton.style.display = "block";
         goBack.style.display = "none";
+        goBack2.style.display = "none";
         clickIndicator.style.width = "10%";
         intro.src = "intro.jpg";
+        validTicket.style.opacity = 0;
     }, 50);
 });
 
+goBack2.addEventListener("click", () => {
+    setTimeout(() => {
+        statsHider.style.backgroundColor = "#1f1e23";
+        fullscreenButton.style.display = "block";
+        goBack.style.display = "none";
+        goBack2.style.display = "none";
+        clickIndicator.style.width = "10%";
+        intro.src = "intro.jpg";
+        validTicket.style.opacity = 0;
+    }, 50);
+});
 
