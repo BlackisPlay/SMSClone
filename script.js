fb2a9cee0694fc0c1ba1a5d66fa5cb54a8dee316
BlackisPlay <102598445+BlackisPlay@users.noreply.github.com>
2026-03-12 17:01:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let chatSelector = document.getElementById("chatSelector");
 let clickIndicator = document.getElementById("clickIndicator");
+let backgroundImage = document.getElementById("intro");
+let fullscreenButton = document.getElementById("goFullscreen");
+let statsHider = document.getElementById("statsHider");
+let goBack = document.getElementById("goBack");
 
 function startClickedAnimation(){
     clickIndicator.style.opacity = 0.2;
@@ -9,7 +13,21 @@ function startClickedAnimation(){
 chatSelector.addEventListener("click", () => {
     startClickedAnimation();
     setTimeout(() => {
-        window.location.href = "dpmkchat.html"
+        statsHider.style.backgroundColor = "#1f2022";
+        clickIndicator.style.opacity = 0;
+        fullscreenButton.style.display = "none";
+        goBack.style.display = "block";
+        intro.src = "chat.jpg";
+    }, 200);
+});
+
+goBack.addEventListener("click", () => {
+    setTimeout(() => {
+        statsHider.style.backgroundColor = "#1f1e23";
+        fullscreenButton.style.display = "block";
+        goBack.style.display = "none";
+        clickIndicator.style.width = "10%";
+        intro.src = "intro.jpg";
     }, 200);
 });
 
